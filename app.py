e97d1df8a0f7df0c72a7f159cc560cfc842de94e
ViniElias <viniciuseliascr@gmail.com>
2024-04-25 09:08:11 -0300

    add peixes.html

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask("__name__")
 def index():
     return render_template("index.html")
 
+@app.route("/peixes")
+def peixes():
+    return render_template("peixes.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
